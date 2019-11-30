adb1057732535640d76f39172ad6232af1b72aa2
Anik <anik@agiletech.com>
2019-11-30 12:27:53 -0500

    5th class running

---
+++ basicJava/src/seleniumTest/SeleniumRun.java
@@ -0,0 +1,30 @@
+package seleniumTest;
+
+public class SeleniumRun {
+
+	public static void main(String[] args) {
+		
+		// how to use a function 
+		// creating an object of the seleniumTest class
+		
+		SeleniumTest st;
+		st=new SeleniumTest();
+		
+		SeleniumTest st1=new SeleniumTest();
+		
+		st.go(78,"bob");
+		System.out.println("-----------------");
+		st.go(120, "vance");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ basicJava/src/seleniumTest/SeleniumTest.java
@@ -0,0 +1,27 @@
+package seleniumTest;
+
+public class SeleniumTest {
+	
+	
+	// storage of functions 
+	
+	
+	// how to create a function
+	// how to use a function 
+	
+	public String go(int x, String y) {
+		
+		System.out.println(x);
+		System.out.println(y);
+		int z = 65;
+		String abc="john";
+		
+		return abc;
+		
+	}
+	
+	
+	
+	
+
+}
