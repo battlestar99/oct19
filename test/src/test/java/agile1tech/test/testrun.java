c5b86ffe52d7322a59102424fa3e341a76e5e4ec
Anik <anik@agiletech.com>
2020-01-11 13:01:17 -0500

    New maven project is created

---
+++ test/src/test/java/agile1tech/test/testrun.java
@@ -0,0 +1,15 @@
+package agile1tech.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testrun {
+
+	public static void main(String[] args) {
+		
+		
+		WebDriver driver=new ChromeDriver(); 
+
+	}
+
+}
